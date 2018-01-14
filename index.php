88070b3449a28224b6f3b0ca3a9f0432580c62a2
BrightSkyz <BrightSkyz@users.noreply.github.com>
2018-01-14 07:58:00 -0500

    Changed Indent and Attempted to Fix styling on navbar

--- index.php
+++ index.php
@@ -3,22 +3,22 @@
 	$page_subhead = "The simple, sleek, and powerful Minecraft server management panel.";
 	include './include/header.php';
 ?>
-    <div class="container">
-		<div class="row">
-			<div class="col-4">
-				<h2>Simple</h2>
-				<p>With our easy to understand panel, you won't have trouble finding out how to do things. Everything on the panel is straight forward to make your life easy.</p>
-			</div>
-			<div class="col-4">
-				<h2>Sleek</h2>
-				<p>With <a href="https://getbootstrap.com">Bootstrap 4</a> and <a href="https://bootswatch.com">Bootswatch</a> your panel will look great. The panel isn't cluttered with buttons, and is easy to understand.</p>
-			</div>
-			<div class="col-4">
-				<h2>Powerful</h2>
-				<p>We keep our panel very powerful to make it stand up to the competition. We strive to provide what the competitors have and more. Our paenel may look basic, but it isn't at all.</p> 
-			</div>
+<div class="container">
+	<div class="row">
+		<div class="col-4">
+			<h2>Simple</h2>
+			<p>With our easy to understand panel, you won't have trouble finding out how to do things. Everything on the panel is straight forward to make your life easy.</p>
+		</div>
+		<div class="col-4">
+			<h2>Sleek</h2>
+			<p>With <a href="https://getbootstrap.com">Bootstrap 4</a> and <a href="https://bootswatch.com">Bootswatch</a> your panel will look great. The panel isn't cluttered with buttons, and is easy to understand.</p>
+		</div>
+		<div class="col-4">
+			<h2>Powerful</h2>
+			<p>We keep our panel very powerful to make it stand up to the competition. We strive to provide what the competitors have and more. Our paenel may look basic, but it isn't at all.</p>
 		</div>
 	</div>
+</div>
 <?php
 	include './include/footer.php';
 ?>
