e974872b806b57ef9df68717e28b08a9ec28ad2d
BrightSkyz <BrightSkyz@users.noreply.github.com>
2018-01-28 20:44:55 -0500

    Fixed spacing on download.php

--- download.php
+++ download.php
@@ -4,14 +4,18 @@
 	include './include/header.php';
 ?>
 <div class="container-fluid">
-	<h2>How to install StartPanel?</h2>
-	<p>To install StartPanel, first make sure you meet the requirements listed below.</p>
-	<h3>Requirements</h3>
-	<ul>
-		<li>Fresh Ubuntu 16.04 install</li>
-		<li>SSH access</li>
-		<li>Some time</li>
-	</ul>
+	<div class="row">
+		<div class="col-12">
+			<h2>How to install StartPanel?</h2>
+			<p>To install StartPanel, first make sure you meet the requirements listed below.</p>
+			<h3>Requirements</h3>
+			<ul>
+				<li>Fresh Ubuntu 16.04 install</li>
+				<li>SSH access</li>
+				<li>Some time</li>
+			</ul>
+		</div>
+	</div>
 </div>
 <?php
 	include './include/footer.php';
